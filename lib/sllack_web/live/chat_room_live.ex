7f5d561788ec4a0d17d773a2351b59eb7e8496fa
Rajesh Pillai <pillai.rajesh@gmail.com>
2025-09-29 15:17:22 +0530

    15 func components: list all rooms in sidebar: NOTE: Removed app layout for now

--- lib/sllack_web/live/chat_room_live.ex
+++ lib/sllack_web/live/chat_room_live.ex
@@ -5,8 +5,9 @@ defmodule SllackWeb.ChatRoomLive do
   alias Sllack.Repo
 
   def mount(_params, _session, socket) do
-    room = Room |> Repo.all() |> List.first()
-    {:ok, assign(socket, room: room, hide_topic?: false)}
+    rooms = Repo.all(Room)
+    room = List.first(rooms)
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle_topic", _value, socket) do
@@ -16,25 +17,63 @@ defmodule SllackWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Sllack
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col grow shadow-lg">
       <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-sm font-bold leading-none">
-            {@room.name}
+            #{@room.name}
           </h1>
           <div
             class={["text-xs leading-none h-3.5", @hide_topic? && "text-slate-600"]}
-            phx-click="toggle_topic">
+            phx-click="toggle-topic"
+          >
             <%= if @hide_topic? do %>
               [Topic hidden]
             <% else %>
-              <%= @room.topic %>
+              {@room.topic}
             <% end %>
           </div>
         </div>
       </div>
     </div>
-
     """
   end
+
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        {@room.name}
+      </span>
+    </a>
+    """
+  end
+
 end
