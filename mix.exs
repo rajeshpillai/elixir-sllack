e239b54594f552e868c59a19efe578c80b6df3b5
Rajesh Pillai <pillai.rajesh@gmail.com>
2025-09-30 15:28:29 +0530

    36. External dependencies - add timestamps formatted with Timex

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule Sllack.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:timex, "~> 3.7" }
     ]
   end
 
